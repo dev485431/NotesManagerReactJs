bb2e2af9a795457322b634ebf47844f884020fbf
Tomasz Krzos <tkrzos@dataart.com>
2017-02-22 17:51:00 +0100

    React DND - initial contexts setup /app not working at the moment!/

--- react-app/components/NoteList/NoteList.js
+++ react-app/components/NoteList/NoteList.js
@@ -1,12 +1,71 @@
 import React from "react"
 import _ from "lodash"
 import {Modal, Button} from "react-bootstrap";
+import {DragSource, DropTarget} from 'react-dnd';
+
+import flow from 'lodash/flow';
 
 import Note from "./Note";
 import {LOADED} from "../../constants/noteListState"
 
+//todo: export to constants
+const ItemTypes = {
+    NOTE: 'note'
+};
 
-export default class NoteList extends React.Component {
+/**
+ * Specifies the drag source contract.
+ * Only `beginDrag` function is required.
+ */
+const noteSource = {
+    beginDrag(props) {
+        // Return the data describing the dragged item
+        const item = {id: props.id};
+        return item;
+    },
+
+    endDrag(props, monitor, component) {
+        if (!monitor.didDrop()) {
+            return;
+        }
+
+        // When dropped on a compatible target, do something
+        const item = monitor.getItem();
+        const dropResult = monitor.getDropResult();
+        //CardActions.moveCardToList(item.id, dropResult.listId);
+    }
+};
+
+/**
+ * Specifies which props to inject into your component.
+ */
+function collectDrag(connect, monitor) {
+    return {
+        // Call this function inside render()
+        // to let React DnD handle the drag events:
+        connectDragSource: connect.dragSource(),
+        // You can ask the monitor about the current drag state:
+        isDragging: monitor.isDragging()
+    };
+}
+
+
+const squareTarget = {
+    drop(props) {
+        //moveKnight(props.x, props.y);
+    }
+};
+
+
+function collectDrop(connect, monitor) {
+    return {
+        connectDropTarget: connect.dropTarget(),
+        isOver: monitor.isOver()
+    };
+}
+
+
+class NoteList extends React.Component {
 
     static propTypes = {
         noteList: React.PropTypes.object.isRequired,
@@ -40,3 +99,10 @@ export default class NoteList extends React.Component {
         )
     }
 }
+
+//todo: ItemType in separate file?
+
+export default flow(
+    DragSource(ItemTypes.NOTE, noteSource, collectDrag),
+    DropTarget(ItemTypes.NOTE, squareTarget, collectDrop)
+)(NoteList);
