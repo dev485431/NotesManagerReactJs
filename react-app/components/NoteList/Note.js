5389828202f754c73ba837d6e86b2d7360ae9589
Tomasz Krzos <tkrzos@dataart.com>
2017-02-23 14:50:05 +0100

    React DND - initial working version; needs instant backend position save and use of reducer for setting notes with new positions;

--- react-app/components/NoteList/Note.js
+++ react-app/components/NoteList/Note.js
@@ -1,7 +1,10 @@
 import React from "react"
+import ReactDOM from 'react-dom';
 import _ from "lodash"
 import {OverlayTrigger, Tooltip} from "react-bootstrap";
 import InlineEdit from 'react-edit-inline';
+
+import {DragSource, DropTarget} from 'react-dnd';
 
 import NoteDetails from "./NoteDetails";
 
@@ -9,13 +12,62 @@ import {NOTE_TOOLTIP_PREVIEW_MAX} from "../../constants/appSettings"
 import {NOTE_TITLE_MIN, NOTE_TITLE_MAX} from "../../constants/appSettings"
 
 
-export default class Note extends React.Component {
+const ItemTypes = {
+    NOTE: 'note',
+};
+
+const noteSource = {
+    beginDrag(props) {
+        return {
+            id: props.id,
+            index: props.index,
+        };
+    },
+};
+
+const noteTarget = {
+    hover(props, monitor, component) {
+        const dragIndex = monitor.getItem().index;
+        const hoverIndex = props.index;
+        console.log(hoverIndex)
+
+        if (dragIndex === hoverIndex) {
+            return;
+        }
+
+        const hoverBoundingRect = ReactDOM.findDOMNode(component).getBoundingClientRect();
+        const hoverMiddleX = (hoverBoundingRect.right - hoverBoundingRect.left) / 2;
+        const clientOffset = monitor.getClientOffset();
+        const hoverClientX = clientOffset.x - hoverBoundingRect.left;
+
+        //move left only when dragged on the left of hover
+        if (dragIndex > hoverIndex && hoverClientX > hoverMiddleX) {
+            return;
+        }
+        //move right only when dragged on the right of hover
+        if (dragIndex < hoverIndex && hoverClientX < hoverMiddleX) {
+            return;
+        }
+
+        props.moveNote(dragIndex, hoverIndex);
+        monitor.getItem().index = hoverIndex;
+    },
+};
+
+class Note extends React.Component {
 
     static propTypes = {
         note: React.PropTypes.object.isRequired,
         activeNoteId: React.PropTypes.number,
         setActiveNote: React.PropTypes.func.isRequired,
-        updateNote: React.PropTypes.func.isRequired
+        updateNote: React.PropTypes.func.isRequired,
+
+        connectDragSource: React.PropTypes.func.isRequired,
+        connectDropTarget: React.PropTypes.func.isRequired,
+        index: React.PropTypes.number.isRequired,
+        isDragging: React.PropTypes.bool.isRequired,
+        id: React.PropTypes.any.isRequired,
+        moveNote: React.PropTypes.func.isRequired,
     }
 
     constructor(props) {
@@ -62,8 +114,11 @@ export default class Note extends React.Component {
             </div>
         }
 
+        const {isDragging, connectDragSource, connectDropTarget} = this.props;
+        const opacity = isDragging ? 0 : 1;
+
         let selectedClass = isActiveNote ? " active-note-list" : "";
-        return (
+        return connectDragSource(connectDropTarget((
             <li className={"element-note-list" + selectedClass}>
                 <div>
                     <OverlayTrigger placement="top" overlay={tooltip}>
@@ -85,7 +140,17 @@ export default class Note extends React.Component {
                 </div>
                 {noteDetails}
             </li>
-        )
+        )));
 
     }
 }
+
+export default _.flow([
+    DragSource(ItemTypes.NOTE, noteSource, (connect, monitor) => ({
+        connectDragSource: connect.dragSource(),
+        isDragging: monitor.isDragging(),
+    })),
+    DropTarget(ItemTypes.NOTE, noteTarget, connect => ({
+        connectDropTarget: connect.dropTarget(),
+    }))]
+)(Note);
--- react-app/components/NoteList/NoteList.js
+++ react-app/components/NoteList/NoteList.js
@@ -1,12 +1,16 @@
 import React from "react"
+import update from 'react/lib/update';
 import _ from "lodash"
 import {Modal, Button} from "react-bootstrap";
+
+import HTML5Backend from 'react-dnd-html5-backend';
+import {DragDropContext} from 'react-dnd';
 
 import Note from "./Note";
 import {LOADED} from "../../constants/noteListState"
 
 
-export default class NoteList extends React.Component {
+class NoteList extends React.Component {
 
     static propTypes = {
         noteList: React.PropTypes.object.isRequired,
@@ -17,16 +21,39 @@ export default class NoteList extends React.Component {
 
     constructor(props) {
         super(props)
+        this.state = {
+            notes: _.sortBy(this.props.noteList.notes, note => note.position)
+        }
+    }
+
+    componentWillReceiveProps = (nextProps) => {
+        this.setState({
+            notes: _.sortBy(nextProps.noteList.notes, note => note.position)
+        })
+    }
+
+    moveNote = (dragIndex, hoverIndex) => {
+        const dragNote = this.state.notes[dragIndex];
+
+        this.setState(update(this.state, {
+            notes: {
+                $splice: [
+                    [dragIndex, 1],
+                    [hoverIndex, 0, dragNote],
+                ],
+            },
+        }));
     }
 
     render() {
         let notes = [];
         if (this.props.noteList.status == LOADED) {
-            let folderNotes = _.filter(this.props.noteList.notes, el => el.directoryId == this.props.activeFolderId);
+            let folderNotes = _.filter(this.state.notes, el => el.directoryId == this.props.activeFolderId);
             folderNotes.map(note => {
                 notes.push(
                     <Note key={note.id} note={note} activeNoteId={this.props.noteList.activeNoteId}
-                          setActiveNote={this.props.setActiveNote} updateNote={this.props.updateNote}/>
+                          setActiveNote={this.props.setActiveNote} updateNote={this.props.updateNote}
+                          id={note.id} index={note.position} moveNote={this.moveNote}/>
                 )
             })
         }
@@ -40,3 +67,5 @@ export default class NoteList extends React.Component {
         )
     }
 }
+
+export default DragDropContext(HTML5Backend)(NoteList);
