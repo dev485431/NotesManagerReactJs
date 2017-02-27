dacb2699a0dfa62fc574280d7f54630fed69e1da
Tomasz Krzos <tkrzos@dataart.com>
2017-02-27 15:25:09 +0100

    Minor improvements;

--- react-app/containers/App.js
+++ react-app/containers/App.js
@@ -14,25 +14,16 @@ import AddFolderPage from "./AddFolderPage"
 import AddNotePage from "./AddNotePage"
 import NotFound404 from "../components/NotFound404"
 
-// function without params to get initial/preloaded state and pass to the store
-// there should be reducers for each initial state object's keys !!!
-// otherwise keys will be ignored and not put into the state
-let getInitState = () => {
-
-}
-
 const store = createStore(
     combineReducers({
         ...reducers,
         routing: routerReducer
     }),
-    getInitState(),
     //todo: remove in final version
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 
-// Create an enhanced history that syncs navigation events with the store
 const history = syncHistoryWithStore(hashHistory, store);
 
 class App extends React.Component {
@@ -40,8 +31,6 @@ class App extends React.Component {
         super(props);
     }
 
-    // routes for particular views/containers
-    //  tell the router to use our enhanced history
     render() {
         return (
             <Provider store={store}>
@@ -49,7 +38,7 @@ class App extends React.Component {
                     <Route path="/" component={MainPage}/>
                     <Route path="/addFolder" component={AddFolderPage}/>
                     <Route path="/addNote" component={AddNotePage}/>
-                    <Route path="*" status={404} component={NotFound404}/>
+                    <Route path="*" component={NotFound404}/>
                 </Router>
             </Provider>
         )
